3374f28b80d333e7bb05851fc6c8aa324e56a065
Vlad401-del <vladfromsoara@student.ub.ac.id>
2025-09-22 20:25:32 +0700

    Full

--- app/src/main/java/com/example/halamanregister/MainActivity.kt
+++ app/src/main/java/com/example/halamanregister/MainActivity.kt
@@ -22,8 +22,17 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberDatePickerState
+import android.widget.Toast
+import androidx.compose.material3.Divider
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.halamanregister.ui.theme.HalamanRegisterTheme
+import kotlin.math.tan
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +44,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background)
                 {
-                    //kalkulator nanti
                     RegisterPage()
                 }
             }
@@ -43,6 +51,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterPage() {
     Column(
@@ -50,11 +59,23 @@ fun RegisterPage() {
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
-            text = "Halo",
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth()
+            text = "Darryl M. Immanuel Panggabean",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Normal,
+            // textAlign = TextAlign.Center
+        )
+        Text(
+            text = "245150400111056",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Normal
+        )
+
+        Divider(modifier = Modifier.padding(vertical = 8.dp))
+
+        Text(
+            text = "Halo, Selamat Datang",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
         )
 
         var nama by remember { mutableStateOf("") }
@@ -74,5 +95,61 @@ fun RegisterPage() {
             label = {Text("NIM")},
             modifier = Modifier.fillMaxWidth()
         )
+
+        var email by remember { mutableStateOf("") }
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = {Text("Email")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        var tanggalLahir by remember { mutableStateOf("Pilih tanggal lahir") }
+        var showDialog by remember { mutableStateOf(false) }
+
+        Button(onClick = {showDialog = true}, modifier = Modifier.fillMaxWidth()) {
+            Text(text = tanggalLahir)
+        }
+
+        if (showDialog) {
+            val datePickerState = rememberDatePickerState()
+            DatePickerDialog(
+                onDismissRequest = {showDialog = false},
+                confirmButton = {
+                    Button(onClick = {
+                        datePickerState.selectedDateMillis?.let {
+                            val selectedDate = java.util.Calendar.getInstance().apply{
+                                timeInMillis = it
+                            }
+                            val day = selectedDate.get(java.util.Calendar.DAY_OF_MONTH)
+                            val month = selectedDate.get(java.util.Calendar.MONTH)
+                            val year = selectedDate.get(java.util.Calendar.YEAR)
+                            tanggalLahir = "$day/${month + 1}/$year"
+                        }
+                        showDialog = false
+                    }) {
+                        Text("OK")
+                    }
+                },
+                dismissButton = {
+                    Button(onClick = {showDialog = false}) {
+                        Text("Cancel")
+                    }
+                }
+            ) {
+                DatePicker(state = datePickerState)
+            }
+        }
+
+//        val context = LocalContext.current
+//        Button(onClick = {
+//            Toast.makeText(
+//                context,
+//                "Data diterima: Nama = $nama, NIM = $nim, Tanggal Lahir = $tanggalLahir",
+//                Toast.LENGTH_LONG
+//            ).show()
+//        }, modifier = Modifier.fillMaxWidth()
+//            ) { Text("Daftar") }
     }
 }
